582912af52cbd925bc3216d81938f4209790a765
Alejandrorg5 <alejandro.rodriguez.g1@estudiantat.upc.edu>
2025-02-28 09:52:50 +0100

    Añadido movimiento super basico + camara

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include "raylib.h"
+#include "raymath.h"
 #include <iostream>
 
 using namespace std;
@@ -6,10 +7,9 @@ using namespace std;
 //----------------------------------------------------------------------------------
 // Some Defines y ENUMS
 //----------------------------------------------------------------------------------
-#define MAX_TUBES 100
-#define FLOPPY_RADIUS 24
-#define TUBES_WIDTH 80
 #define BACKGROUND "../../resources/world/World_1_1.png"
+#define PLAYER_JUMP_SPD 350.0f
+#define GRAVEDAD 400
 
 enum Powers {
     BASE = 0,
@@ -22,38 +22,27 @@ enum Powers {
 // Class Definition
 //----------------------------------------------------------------------------------
 
-
-
 class Mario {
-private:
-    float velocidad;
-    Powers poder = BASE;
-
 public:
-    /* Getters y Setters */
-
-    int GetVelocidad() {
-        return velocidad;
-    }
-
-    void SetVelocidad(int valor) {
-        velocidad = valor;
-    }
-
-    Powers GetPower() {
-        return poder;
-    }
-
-    void SetPower(Powers valor) {
-        poder = valor;
-    }
+    Rectangle position;
+    bool canJump = true;
+    float velocidad;
+    Powers poder;
 
     /* Constructores */
+    Mario();
+    
+    Mario(float posX, float posY) {
+        position = { posX, posY, 24, 32 };
+        velocidad = 0;
+        poder = BASE;
+    }
+};
 
-    Mario(float vel, Powers pow) {
-        velocidad = vel;
-        poder = pow;
-    };
+struct Hitbox {
+    Rectangle rect;
+    int blocking;
+    Color color;
 };
 
 //------------------------------------------------------------------------------------
@@ -65,16 +54,18 @@ static const int screenHeight = 480;
 static bool gameOver = false;
 static int score = 0;
 
+
+
+Camera2D camera = { 0 };
 Texture2D background;
 
 //------------------------------------------------------------------------------------
 // Module Functions Declaration (local)
 //------------------------------------------------------------------------------------
 static void InitGame(void);         // Initialize game
-static void UpdateGame(void);       // Update game (one frame)
-static void DrawGame(void);         // Draw game (one frame)
+static void UpdateGame(Mario* mario, Hitbox* hitboxes, float delta);       // Update game (one frame)
+static void DrawGame(Mario* mario, Hitbox* hitboxes);         // Draw game (one frame)
 static void UnloadGame(void);       // Unload game
-static void UpdateDrawFrame(void);  // Update and Draw (one frame)
 
 //------------------------------------------------------------------------------------
 // Program main entry point
@@ -84,26 +75,41 @@ int main(void)
     // Initialization
     //---------------------------------------------------------
     InitWindow(screenWidth, screenHeight, "classic game: Super Mario Bros.");
-    background = LoadTexture(BACKGROUND);
-    InitGame();
+    background = LoadTexture(BACKGROUND); //Cargo la textura del background
 
+    Mario mario(380, 380); //Creo el objeto de Mario
+    Hitbox lista_hitboxes[] = {
+        { {0, 416, 1000, 400}, 1, BLUE }
+    }; // Creo la lista de todas las colisiones
+
+    int envItemsLength = 1;
+
+
+    score = 0;
+    gameOver = false;
+
+
+    camera.target = { mario.position.x + 20.0f, mario.position.y + 20.0f };
+    camera.offset = { mario.position.x, mario.position.y };
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
 
     SetTargetFPS(60);
+
     //--------------------------------------------------------------------------------------
 
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update and Draw
-        //----------------------------------------------------------------------------------
-        UpdateDrawFrame();
-        //----------------------------------------------------------------------------------
+        float deltaTime = GetFrameTime();
+
+        UpdateGame(&mario, lista_hitboxes, deltaTime);
+        DrawGame(&mario, lista_hitboxes);
     }
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
     UnloadGame();         // Unload loaded data (textures, sounds, models...)
-
     CloseWindow();        // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
@@ -116,19 +122,27 @@ int main(void)
 // Initialize game variables
 void InitGame(void)
 {
-    score = 0;
-    gameOver = false;
+   
 
-
+   
 
 }
 
 // Update game (one frame)
-void UpdateGame(void)
+void UpdateGame(Mario *mario, Hitbox *hitboxes, float delta)
 {
+
     if (!gameOver)
     {
-       
+        if (IsKeyDown(KEY_RIGHT)) mario->position.x += 5;
+        else if (IsKeyDown(KEY_LEFT)) mario->position.x -= 5;
+
+        if (IsKeyDown(KEY_SPACE) ) {
+            mario->position.y -= 10;
+        }
+      
+        // Camera target follows player
+        camera.target = { mario->position.x + 20, mario->position.y + 20 };
     }
     else
     {
@@ -141,11 +155,13 @@ void UpdateGame(void)
 }
 
 // Draw game (one frame)
-void DrawGame(void)
+void DrawGame(Mario* mario, Hitbox* hitboxes)
 {
     BeginDrawing();
 
     ClearBackground(RAYWHITE);
+
+    BeginMode2D(camera);
 
     /* Dibujado del Background */
     Rectangle initial_position = {0, 0, background.width, background.height };
@@ -153,15 +169,22 @@ void DrawGame(void)
     DrawTexturePro(background, initial_position, final_position, { 0, (float)(background.height / 2) }, 0, WHITE);
     /***************************/
 
-    
+    /* Dibujado de Mario */
+    DrawRectangleRec(mario->position, RED);
+
+    /* Dibujado de Hitbox */
+    for (int i = 0; i < 1; i++) 
+        DrawRectangleRec(hitboxes[i].rect, hitboxes[i].color);
+
+    EndMode2D();
+
     EndDrawing();
 }
 
-// Update and Draw (one frame)
-void UpdateDrawFrame(void)
+void UpdateCameraCenter(Camera2D* camera, Mario* mario, Hitbox* envItems, int envItemsLength, float delta, int width, int height)
 {
-    UpdateGame();
-    DrawGame();
+    camera->offset = { width / 2.0f, height / 2.0f };
+    camera->target = { mario->position.x, mario->position.y };
 }
 
 // Unload game variables
