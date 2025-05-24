6f198a34a4641d01a8d2b4ca81643fea7bfc2145
abelnunez5 <abel.nunez@estudiantat.upc.edu>
2025-05-24 21:04:55 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,15 +15,15 @@ namespace fs = filesystem;
 
 #pragma region DEFINES y ENUMS
 
-#define BACKGROUND  "../../resources/world/Empty_World_1_1.png"
-#define SPRITESHEET "../../resources/sprites/NES - Super Mario Bros - Mario & Luigi.png"
-#define ENEMIES     "../../resources/sprites/NES - Super Mario Bros - Enemies & Bosses.png"
-#define BLOCKS     "../../resources/sprites/NES - Super Mario Bros - Item and Brick Blocks.png"
-#define SOUNDS      "../../resources/Super Mario Bros Efects"
-#define MUSICS      "../../resources/Super Mario Bros Music"
-#define INICIALPAGE "../../resources/NES - Super Mario Bros - Title Screen HUD and Miscellaneous (1).png"
+#define BACKGROUND  "resources/world/Empty_World_1_1.png"
+#define SPRITESHEET "resources/sprites/NES - Super Mario Bros - Mario & Luigi.png"
+#define ENEMIES     "resources/sprites/NES - Super Mario Bros - Enemies & Bosses.png"
+#define BLOCKS     "resources/sprites/NES - Super Mario Bros - Item and Brick Blocks.png"
+#define SOUNDS      "resources/Super Mario Bros Efects"
+#define MUSICS      "resources/Super Mario Bros Music"
+#define INICIALPAGE "resources/NES - Super Mario Bros - Title Screen HUD and Miscellaneous (1).png"
 
-#define PLAYER_JUMP_SPD 500.0f
+#define PLAYER_JUMP_SPD 200.0f
 #define GRAVEDAD 700
 #define ACELERACION 0.5f
 #define VELOCIDAD_MAXIMA 5.0f
@@ -206,7 +206,7 @@ int framesSpeed = 3;
 #pragma region Inits
 
 void InitGrid(vector <Hitbox> *lista_hitboxes) {
-    Image tile_map = LoadImage("../../resources/world/Tile_Map.png");
+    Image tile_map = LoadImage("resources/world/Tile_Map.png");
     Color* colors = LoadImageColors(tile_map);
     for (int x = 0; x < 211; x++)
     {
